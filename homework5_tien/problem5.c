d730eb776c385bbf1e99cf7a59ca8d24f7eab3a9
tiennll <leductiendung29@gmail.com>
2022-10-30 17:38:23 -0500

    asd

--- homework5_tien/problem5.c
+++ homework5_tien/problem5.c
@@ -7,22 +7,31 @@ int main(void)
     int x=0;
 
     printf("Welcome to planet Numerion!");
-
+printf("\nEnter an integer N or -1 to quit: ");
+        scanf("%d", &N);
     do
     {
-        printf("\nEnter an integer N or -1 to quit: ");
-        scanf("%d", &N);
+
         while(i<N)
         {
             if(N == i*i + i)
             {
                 printf("%d is a holy number in Numerion, because %d = %d * %d + %d ", N, N, i,i,i);
                 x=1;
+
             }
             i=i+1;
         }
+        if(x==0)
+        {
+            printf("%d is not a holy number in Numerion" ,N);
+
+        }
+
+    printf("\nEnter an integer N or -1 to quit: ");
+        scanf("%d", &N);
     }while(N > 0);
-    printf("EXITING... HAVE A GOOD DAY!");
+    printf("\nEXITING... HAVE A GOOD DAY!");
 
     return 0;
 }
