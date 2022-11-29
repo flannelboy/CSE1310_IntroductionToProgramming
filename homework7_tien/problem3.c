b5edd3bb4591bc078836d3f92a1d44ea6c4b9ac9
tiennll <leductiendung29@gmail.com>
2022-11-28 23:14:04 -0600

    created hw7 and finished problem 3

---
+++ homework7_tien/problem3.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#define ROWS 4
+#define COLS 3
+
+int generateMtx(int mtx[ROWS][COLS])
+{
+    int value = 0;
+
+    for (int r = 0; r < 4; r++)
+    {
+        for (int c = 0; c < 3; c++)
+        {
+            printf("Enter row %d, column %d: ", r,c);
+            scanf("%d", &value);
+            mtx [r][c] = value;
+        }
+    }
+
+    return mtx;
+}
+int matrixSmallest(int mtx[ROWS][COLS])
+{
+    int oldR = 0;
+    int oldC = 0;
+    int smallest = 0;
+    for (int r = 0; r < 4; r++)
+    {
+        for (int c = 0; c < 3; c++)
+        {
+            if(mtx[r][c] <= mtx[oldR][oldC])
+            {
+                smallest = mtx[r][c];
+            }
+            oldC = c;
+        }
+        oldR = r;
+    }
+    return smallest;
+}
+
+int matrixLargest(int mtx[ROWS][COLS])
+{
+    int oldR = 0;
+    int oldC = 0;
+    int largest = 0;
+    for (int r = 0; r < 4; r++)
+    {
+
+        for (int c = 0; c < 3; c++)
+        {
+
+            if(mtx[r][c] > mtx[oldR][oldC])
+            {
+                largest = mtx[r][c];
+            }
+            oldC = c;
+        }
+        oldR = r;
+    }
+    return largest;
+}
+int elementPosition(int mtx[ROWS][COLS],  int num, int position[2])
+{
+    for (int r = 0; r < 4; r++)
+    {
+        for (int c = 0; c < 3; c++)
+        {
+            if(num == mtx[r][c])
+            {
+                position[0] = r;
+                position[1] = c;
+            }
+        }
+    }
+    return position;
+}
+
+int main()
+{
+    int mtx[ROWS][COLS];
+    generateMtx(mtx);
+    for (int r = 0; r < 4; r++)
+    {
+        for (int c = 0; c < 3; c++)
+        {
+            printf("%3d", mtx[r][c]);
+        }
+        printf("\n");
+    }
+
+    int smallest = matrixSmallest(mtx);
+    int smallPosition[2] = {-1, -1};
+    elementPosition(mtx, smallest, smallPosition);
+
+    int largest = matrixLargest(mtx);
+    int largePosition[2] = {-1, -1};
+    elementPosition(mtx, largest, largePosition);
+
+    printf("Largest element: %d\n", largest);
+    printf(" found at row %d, column %d\n", largePosition[0], largePosition[1]);
+    printf("Smallest element: %d\n", smallest);
+    printf(" found at row %d, column %d\n", smallPosition[0], smallPosition[1]);
+    return 0;
+}
