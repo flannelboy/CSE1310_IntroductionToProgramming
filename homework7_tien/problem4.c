4db0a9dfab32ff06de0534b614b6ffdea08b7315
Tien <leductiendung29@gmail.com>
2022-12-01 12:33:52 -0600

    finished hw7

--- homework7_tien/problem4.c
+++ homework7_tien/problem4.c
@@ -2,35 +2,13 @@
 #define ROW 5
 #define COL 3
 
-void columnAverages(float mtx[ROW][COL], float columnAvg[3])
+float columnAverages(float columnSum, float time)
 {
-    float columnSum, time = 0;
-    for(int c = 0; c <= COL; c++)
-    {
-        columnSum = 0;
-        time = 0;
-        for(int r = 0; r <= ROW; r++)
-        {
-            columnSum = columnSum + mtx[r][c];
-            time++;
-        }
-        columnAvg[c] = columnSum / time;
-    }
+    return columnSum / time;
 }
-void rowAverages(float mtx[ROW][COL], float rowAvg[5])
+float rowAverages(float rowSum, float time)
 {
-    float rowSum, time = 0;
-    for(int c = 0; c <= COL; c++)
-    {
-        rowSum = 0;
-        time = 0;
-        for(int r = 0; r <= ROW; r++)
-        {
-            rowSum = rowSum + mtx[r][c];
-            time++;
-        }
-        rowAvg[c] = rowSum / time;
-    }
+    return rowSum / time;
 }
 float regionAverages(float mtx[ROW][COL],int left, int right, int bot, int top)
 {
@@ -70,7 +48,6 @@ void printMtx(float mtx[ROW][COL], float rowAverages[5])
             printf("%8.1f", mtx[r][c]);
         }
     }
-
     printf("\n\n        Col 0   Col 1   Col 2     Avg");
     for(int r = 0; r < ROW; r++)
     {
@@ -81,27 +58,40 @@ void printMtx(float mtx[ROW][COL], float rowAverages[5])
         }
         printf("%8.1f", rowAverages[r]);
     }
-
 }
 int main()
 {
     int left, right, bot, top;
-
     float mtx[ROW][COL]={};
     generateMtx(mtx);
-
-
-
-    float rowAvg[5] = {1,1,1,1,1};
-    rowAverages(mtx, rowAvg);
-
+    float columnAvg[COL] = {};
+    float columnSum, time = 0;
+    for(int c = 0; c < COL; c++)
+    {
+        columnSum = 0;
+        time = 0;
+        for(int r = 0; r < ROW; r++)
+        {
+            columnSum = columnSum + mtx[r][c];
+            time++;
+        }
+        columnAvg[c] = columnAverages(columnSum, time);
+    }
+    float rowAvg[ROW] = {};
+    float rowSum = 0;
+    for(int r = 0; r < ROW; r++)
+    {
+        rowSum = 0;
+        time = 0;
+        for(int c = 0; c < COL; c++)
+        {
+            rowSum = rowSum + mtx[r][c];
+            time++;
+        }
+        rowAvg[r] = rowAverages(rowSum, time);
+    }
     printMtx(mtx, rowAvg);
-
-    float columnAvg[3] = {1,1,1};
-    columnAverages(mtx, columnAvg);
-
     printf("\n  Avg%8.1f%8.1f%8.1f", columnAvg[0],columnAvg[1],columnAvg[2]);
-
     printf("\nEnter top region boundary: ");
         scanf("%d", &top);
     printf("Enter bottom region boundary: ");
@@ -110,7 +100,6 @@ int main()
         scanf("%d", &left);
     printf("Enter right region boundary: ");
         scanf("%d", &right);
-
     double rAvg = regionAverages(mtx, left, right, bot, top);
     printf("Region average: %.1f", rAvg);
 }
